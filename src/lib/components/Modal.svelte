778701a196bc9ce4e780adcb91d6f464a6e37e68
Muspi Merol <wjj123345567@icloud.com>
2024-03-02 13:01:34 +0800

    feat: refactor `<Modal>`

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import { onMount } from "svelte"
+
   export let show: boolean
+
+  export let ssr = false
+
+  let mounted = false
+
+  $: showModal = show && (ssr || mounted)
+
+  onMount(() => mounted = true)
 </script>
 
 <slot name="backdrop">
-  <div class="pointer-events-none fixed inset-0 transition duration-1000" class:backdrop-grayscale={show} />
+  <div class="pointer-events-none absolute inset-0 transition duration-1000" class:backdrop-grayscale={show} />
 </slot>
 
-{#if show}
+{#if showModal}
   <slot>
-    <div class="fixed inset-0 z-100 grid place-items-center">
+    <div class="absolute inset-0 z-100 grid place-items-center">
       <slot name="content" />
     </div>
   </slot>
