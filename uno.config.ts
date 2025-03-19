be333805d8dd838f813ce82f7a6198a0a4365930
Muspi Merol <me@promplate.dev>
2025-03-19 15:41:55 +0800

    fix: replace `presetUno` with `presetWind3` and enable on-demand preflight feature

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,10 @@
 import extractorSvelte from "@unocss/extractor-svelte"
-import { defineConfig, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from "unocss"
+import { defineConfig, presetIcons, presetTypography, presetWebFonts, presetWind3, transformerDirectives, transformerVariantGroup } from "unocss"
 
 const config = defineConfig({
   extractors: [extractorSvelte()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
-  presets: [presetUno(), presetWebFonts({ fonts: { mono: "Fira Code:300,400,500,600,700", fancy: "Bungee" } }), presetIcons(), presetTypography()],
+  presets: [presetWind3({ preflight: "on-demand" }), presetWebFonts({ fonts: { mono: "Fira Code:300,400,500,600,700", fancy: "Bungee" } }), presetIcons(), presetTypography()],
 })
 
 export default config
